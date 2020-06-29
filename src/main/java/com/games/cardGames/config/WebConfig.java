90448bf7beb70485ee939b9b2164a6fdab07b45c
blurryface <andrelopezcollantes21@gmail.com>
2020-06-30 00:08:26 +0200

    commented tests

--- src/main/java/com/games/cardGames/config/WebConfig.java
+++ src/main/java/com/games/cardGames/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("https://blurryface123.github.io")
                 .allowCredentials(false)
                 .maxAge(3600)
                 .allowedHeaders("Accept", "Content-Type", "Origin",
